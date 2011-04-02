05b013d644211e7500fe913075f99c1581f51c12
dfinke <finked@hotmail.com>
2011-04-02 12:09:14 -0400

    Added comment for where to get PowerShell Pack -- DCF

--- WPK/Hello-World.ps1
+++ WPK/Hello-World.ps1
@@ -1,4 +1,4 @@
 # Download PowerShell Pack and install
-
+# http://archive.msdn.microsoft.com/PowerShellPack/Release/ProjectReleases.aspx?ReleaseId=3341
 Import-Module WPK
 New-Label "Hello World" -FontSize 42 -Show
