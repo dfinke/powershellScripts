35e84d36ba96d0dd9c7d73c09c5073603f14279d
dfinke <finked@hotmail.com>
2011-04-05 21:01:43 -0400

    Tweaks -- DCF

--- Calling Excel Functions From PowerShell/Invoke-ExcelFunction.ps1
+++ Calling Excel Functions From PowerShell/Invoke-ExcelFunction.ps1
@@ -1,16 +1,20 @@
 # Calling Excel Math Functions From PowerShell
 
 $xl        = New-Object -ComObject Excel.Application
-$xlprocess = ps excel
+$xlprocess = Get-Process excel
 
-$x    = $xl.WorksheetFunction
+$wf   = $xl.WorksheetFunction
 $data = 1,2,3,4
 $m    = ((1,2,3),(4,5,6),(7,8,9))
 
-$x.Median($data)
-$x.StDev($data)
-$x.Var($data)
-$x.MInverse($m)
+Write-Host -ForegroundColor green Median
+$wf.Median($data)
+Write-Host -ForegroundColor green StDev
+$wf.StDev($data)
+Write-Host -ForegroundColor green Var
+$wf.Var($data)
+Write-Host -ForegroundColor green MInverse
+$wf.MInverse($m)
 
 $xl.quit()
 $xlprocess | kill
