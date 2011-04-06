8f512978f2b5248e7db67c1e8781ccaa14c19427
dfinke <finked@hotmail.com>
2011-04-05 21:02:35 -0400

    Tweaks -- DCF

--- Calling Excel Functions From PowerShell/Invoke-ExcelFunction.ps1
+++ Calling Excel Functions From PowerShell/Invoke-ExcelFunction.ps1
@@ -9,10 +9,13 @@ $m    = ((1,2,3),(4,5,6),(7,8,9))
 
 Write-Host -ForegroundColor green Median
 $wf.Median($data)
+
 Write-Host -ForegroundColor green StDev
 $wf.StDev($data)
+
 Write-Host -ForegroundColor green Var
 $wf.Var($data)
+
 Write-Host -ForegroundColor green MInverse
 $wf.MInverse($m)
 
